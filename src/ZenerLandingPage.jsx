b14d8f9d03a7ed8a64602a357ad2f856bd103f07
Sabari Thevar <sabarithevarr04@gmail.com>
2025-07-16 12:57:30 +0400

    Initial deploy-ready commit

---
+++ src/ZenerLandingPage.jsx
@@ -0,0 +1,1 @@
+// Placeholder React component for Zener Landing Page
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ZenerLandingPage from './ZenerLandingPage'
+import './index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(<ZenerLandingPage />)
